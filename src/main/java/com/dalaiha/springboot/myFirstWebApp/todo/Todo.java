8c3795238bc649e8121b0154bf368d24ede6c258
dihia <dihiasalhi@gmail.com>
2025-11-23 15:15:14 -0500

    making todo an entity and populate todo data into h2

--- src/main/java/com/dalaiha/springboot/myFirstWebApp/todo/Todo.java
+++ src/main/java/com/dalaiha/springboot/myFirstWebApp/todo/Todo.java
@@ -1,11 +1,21 @@
 package com.dalaiha.springboot.myFirstWebApp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name="Todo"/*if we don't use this param, by default the name is the class's name*/)
+//it allows us to map a bean to database table
+//spring boot autoconfiguration will automatically create this table in the database
 public class Todo {
+    @Id
+    @GeneratedValue
     private int id;
+    @Column(name="username" /* not mandatory param */)
     private String username;
 
     @Size(min=10, message = "needs to be more than 10 charcters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10001, 'didi', 'Get AWS certified', CURRENT_DATE, FALSE);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10002, 'didi', 'Get Spring boot certified', CURRENT_DATE, FALSE);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10003, 'didi', 'Get blabla certified', CURRENT_DATE, FALSE);
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values (10004, 'momo', 'watch the whole saison 6 of the mentalist', CURRENT_DATE, FALSE);
